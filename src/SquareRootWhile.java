39c742ec67c4974bfe0393ff1981b05363f804fb
Sadio4 <sadio.diallo@cdpipelinedevshop.com>
2022-08-25 11:42:25 -0400

    First commit

---
+++ src/SquareRootWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class SquareRootWhile {
+    public static void main(String args[])
+    {
+        System.out.print("Type a non-negative integer:-5 ");
+        System.out.print("Invalid number,try again:-1 ");
+        System.out.print("Invalid number,try again:11 ");
+        System.out.print("The square root of 11 is 3.166 ");
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+
+    }
+
+
+}
