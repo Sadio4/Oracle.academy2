c31bafa4132eb3019e908bb903c9a399d8ba2b95
Sadio4 <sadio.diallo@cdpipelinedevshop.com>
2022-08-25 09:31:23 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        double Temp = Math.min(5,9) + Math.abs(-33);
+        System.out.println(Temp);
+        double Hypotenuse = Math.min(3,3) + Math.abs(-7);
+        System.out.println(Hypotenuse);
+        double Dice = Math.min(2,2) + Math.abs(-7);
+        System.out.println(Dice);
+
+
+
+
+    }
+}
+
